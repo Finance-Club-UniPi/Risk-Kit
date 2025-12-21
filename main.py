ec387024e8872e0996c142fecca7c7523bc12f53
Giorgos <geokaragiann@outlook.com>
2025-12-21 13:14:41 +0200

    *

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ print("hello world!")
 x = 10
 if x > 9:
     print("Yes!")
-<<<<<<< HEAD
 elif x < 0:
     print("No!")
 
 print("hi!")
-=======
-
-for i in range(10):
-    print(i*8+3)
->>>>>>> dev
